f50e8a36e9f89b9a00313d22d240483110aa16e1
Mohit Vuyala <162402147+mohitgitgeek@users.noreply.github.com>
2024-11-02 20:44:41 +0530

    Version - Final

--- employee_payroll.py
+++ employee_payroll.py
@@ -1,92 +1,157 @@
-# Predefined employee records dictionary
-employee_records = {
-    "E001": {
-        "name": "Alice Johnson",
-        "position": "Software Engineer",
-        "salary": "70000"
-    },
-    "E002": {
-        "name": "Bob Smith",
-        "position": "Project Manager",
-        "salary": "90000"
-    },
-    "E003": {
-        "name": "Charlie Brown",
-        "position": "Data Analyst",
-        "salary": "60000"
-    }
-}
+import pickle
+import os
+import pathlib
 
-def add_employee():
-    emp_id = input("Enter Employee ID: ")
-    if emp_id in employee_records:
-        print("Employee ID already exists.")
-        return
-    name = input("Enter Employee Name: ")
-    position = input("Enter Employee Position: ")
-    salary = input("Enter Employee Salary: ")
-    employee_records[emp_id] = {
-        "name": name,
-        "position": position,
-        "salary": salary
-    }
-    print("Employee added successfully.")
+class Account:
+    def __init__(self):
+        self.accNo = 0
+        self.name = ''
+        self.des = ''
+        self.add = ''
+        self.ba = 0
+        self.da = 0
+        self.ta = 0
+        self.hra = 0
+        self.pf = 0
+        self.esi = 0
+        self.gs = 0
+        self.gd = 0
+        self.ns = 0
+    
+    def createAccount(self):
+        self.accNo = int(input("Enter the account no : "))
+        self.name = input("Enter the employer's name : ")
+        self.des = input("Enter the employer's designation: ")
+        self.add = input("Enter the employer's address: ")
+        self.ba = int(input("Enter basic salary of employee (in rupees): "))
+        self.da = int(input("Enter dearness allowance of the employee (in rupees): "))
+        self.ta = int(input("Enter travelling allowance of the employee(in rupees): "))
+        self.hra = int(input("Enter house rent allowance of the employee (in rupees): "))
+        self.gs = self.ba + self.da + self.ta + self.hra
+        self.pf = int(input("Enter provident fund of the employee (in rupees): "))
+        self.esi = int(input("Enter employee state insurance of the employee(in rupees): "))
+        self.gd = self.pf + self.esi
+        self.ns = self.gs - self.gd
+        print("\n\n\nPayroll information stored.\n")
 
-def view_employee():
-    emp_id = input("Enter Employee ID to view: ")
-    if emp_id in employee_records:
-        print(f"ID: {emp_id}")
-        print(f"Name: {employee_records[emp_id]['name']}")
-        print(f"Position: {employee_records[emp_id]['position']}")
-        print(f"Salary: {employee_records[emp_id]['salary']}")
+    def showAccount(self):
+        print("Account Number : ", self.accNo)
+        print("Account Holder Name : ", self.name)
+        print("Designation :", self.des)
+        print("Address:", self.add)
+        print("Basic Salary: ", self.ba)
+        print("Dearness Allowance: ", self.da)
+        print("Travelling Allowance: ", self.ta)
+        print("House Rent Allowance: ", self.hra)
+        print("Gross Salary:", self.gs)
+        print("Provident fund: ", self.pf)
+        print("Employee state insurance : ", self.esi)
+        print("Gross Deduction", self.gd)
+        print("Net salary", self.ns)
+        
+def intro():
+    print("\t\t\t\t**************************")
+    print("\t\t\t\tPAYROLL MANAGEMENT SYSTEM")
+    print("\t\t\t\t**************************")
+
+def writeAccount():
+    account = Account()
+    account.createAccount()
+    writeAccountsFile(account)
+
+def displayAll():
+    file = pathlib.Path("accounts.data")
+    if file.exists():
+        infile = open('accounts.data', 'rb')
+        try:
+            mylist = pickle.load(infile)
+            print()
+            for item in mylist:
+                item.showAccount()
+                print("\n----------------------\n")
+        except EOFError:
+            print("No records to display")
+        infile.close()
     else:
-        print("Employee not found.")
+        print("No records to display")
 
-def update_employee():
-    emp_id = input("Enter Employee ID to update: ")
-    if emp_id in employee_records:
-        name = input("Enter new Employee Name: ")
-        position = input("Enter new Employee Position: ")
-        salary = input("Enter new Employee Salary: ")
-        employee_records[emp_id] = {
-            "name": name,
-            "position": position,
-            "salary": salary
-        }
-        print("Employee updated successfully.")
+def deleteAccount(num):
+    file = pathlib.Path("accounts.data")
+    if file.exists():
+        infile = open('accounts.data', 'rb')
+        try:
+            oldlist = pickle.load(infile)
+        except EOFError:
+            oldlist = []
+        infile.close()
+        newlist = [item for item in oldlist if item.accNo != num]
+        os.remove('accounts.data')
+        with open('accounts.data', 'wb') as outfile:
+            pickle.dump(newlist, outfile)
+        print(f"Account number {num} has been deleted.")
     else:
-        print("Employee not found.")
+        print("No records to delete")
 
-def delete_employee():
-    emp_id = input("Enter Employee ID to delete: ")
-    if emp_id in employee_records:
-        del employee_records[emp_id]
-        print("Employee deleted successfully.")
+def modifyAccount(num):
+    file = pathlib.Path("accounts.data")
+    if file.exists():
+        infile = open('accounts.data', 'rb')
+        try:
+            oldlist = pickle.load(infile)
+        except EOFError:
+            oldlist = []
+        infile.close()
+        os.remove('accounts.data')
+        for item in oldlist:
+            if item.accNo == num:
+                print("Modifying account details for account number:", num)
+                item.createAccount()  # re-uses the input method to modify details
+        with open('accounts.data', 'wb') as outfile:
+            pickle.dump(oldlist, outfile)
+        print(f"Account number {num} has been modified.")
     else:
-        print("Employee not found.")
+        print("No records to modify")
 
-def main_menu():
-    while True:
-        print("\nEmployee Payroll System")
-        print("1. Add Employee")
-        print("2. View Employee")
-        print("3. Update Employee")
-        print("4. Delete Employee")
-        print("5. Exit")
-        choice = input("Enter your choice: ")
-        if choice == "1":
-            add_employee()
-        elif choice == "2":
-            view_employee()
-        elif choice == "3":
-            update_employee()
-        elif choice == "4":
-            delete_employee()
-        elif choice == "5":
-            print("Exiting the system.")
-            break
-        else:
-            print("Invalid choice. Please try again.")
+def writeAccountsFile(account):
+    file = pathlib.Path("accounts.data")
+    if file.exists():
+        infile = open('accounts.data', 'rb')
+        try:
+            oldlist = pickle.load(infile)
+        except EOFError:
+            oldlist = []
+        oldlist.append(account)
+        infile.close()
+    else:
+        oldlist = [account]
+    with open('accounts.data', 'wb') as outfile:
+        pickle.dump(oldlist, outfile)
 
-# Start the payroll system
-main_menu()
+# Start of the program
+ch = ''
+intro()
+
+while ch != '5':
+    print("\tMAIN MENU")
+    print("\t1. NEW ACCOUNT")
+    print("\t2. ALL ACCOUNT HOLDER LIST")
+    print("\t3. CLOSE AN ACCOUNT")
+    print("\t4. MODIFY AN ACCOUNT")
+    print("\t5. EXIT")
+    ch = input("\tSelect Your Option (1-5):") 
+    
+    if ch == '1': 
+        writeAccount()
+    elif ch == '2':
+        displayAll()
+    elif ch == '3':
+        num = int(input("\tEnter the account No. to delete: "))
+        deleteAccount(num)
+    elif ch == '4':
+        num = int(input("\tEnter the account No. to modify: "))
+        modifyAccount(num)
+    elif ch == '5':
+        print("\tThanks for using Payroll Management System")
+        break
+    else:
+        print("Invalid choice.")
